3db388798feca01a956cdb5b1dd1fc81637decad
irfanurislam <mdirfanurislam@gmaile.com>
2023-04-12 17:11:28 +0600

    some statistics changed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,99 +1,92 @@
 import React from 'react';
-import { PieChart, Pie, Tooltip } from "recharts";
+import { PieChart, Pie, Tooltip, ResponsiveContainer } from "recharts";
 const Statistics = () => {
 
   //pie charts assignment ways
  const assignments  = [
         {
           "id": 1,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 1",
+          
+          "name": "Assignment 1",
           "marks": 57
         },
         {
           "id": 2,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 2",
+          
+          "name": "Assignment 2",
           "marks": 59
         },
         {
           "id": 3,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 3",
+          
+          "name": "Assignment 3",
           "marks": 60
         },
         {
           "id": 4,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 4",
+          
+          "name": "Assignment 4",
           "marks": 60
         },
         {
           "id": 5,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 5",
+          
+          "name": "Assignment 5",
           "marks": 37
         },
         {
           "id": 6,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 6",
+          
+          "name": "Assignment 6",
           "marks": 60
         },
         {
           "id": 7,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 7",
+          "name": "Assignment 7",
           "marks": 60
         },
         {
           "id": 8,
-          "name": "Md Irfanur Islam",
-          "assignment_name": "Assignment 8",
+          "name": "Assignment 8",
           "marks": 60
         }
       ]
       
     return (
+      
         <div className='py-28 flex items-center justify-center'>
+         
            <PieChart width={500} height={600} >
-      {/* <Pie
-        data={assignments}
-        dataKey="marks"
-        cx={200}
-        cy={200}
-        outerRadius={150}
-        innerRadius={50}
-        label
-        fill="#8884d8"
-      />  */}
      
      <Pie
         data={assignments}
         dataKey="id"
-        cx={200}
-        cy={200}
+        cx='50%'
+        cy='50%'
         outerRadius={60}
         fill="#8884d8"
         
       />
-      <Tooltip />
+      
       <Pie
         data={assignments}
         dataKey="marks"
-        cx={200}
-        cy={200}
+        cx='50%'
+        cy='50%'
         innerRadius={70}
         outerRadius={120}
         fill="#82ca9d"
         label
       />
+      <Tooltip   />
 
       
-      {/* <Tooltip dataKey='name' /> */}
+     
     
     </PieChart>
+    
         </div>
+  
     );
 };
 
